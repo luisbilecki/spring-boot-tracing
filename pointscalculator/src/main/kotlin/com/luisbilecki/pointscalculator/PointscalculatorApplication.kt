07ade64b85d4f347481805407bce9e7fb19fd36d
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-02-01 22:27:06 -0300

    chore: create initial points calculator ms project

---
+++ pointscalculator/src/main/kotlin/com/luisbilecki/pointscalculator/PointscalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.luisbilecki.pointscalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointscalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<PointscalculatorApplication>(*args)
+}
---
+++ pointscalculator/src/test/kotlin/com/luisbilecki/pointscalculator/PointscalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luisbilecki.pointscalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointscalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
