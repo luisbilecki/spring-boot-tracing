7c513198ef9627c45630921c97d680dfd714e8ad
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-02-14 22:56:20 -0300

    chore: initial commit for api gw project

---
+++ apigateway/src/main/kotlin/com/luisbilecki/apigateway/ApigatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.luisbilecki.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApigatewayApplication>(*args)
+}
