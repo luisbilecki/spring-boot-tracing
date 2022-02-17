70c61d33d73c52e648b31444adf5d676cf0675cc
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-02-16 22:57:29 -0300

    feat: create customer client

---
+++ apigateway/src/main/kotlin/com/luisbilecki/apigateway/client/CustomerClient.kt
@@ -0,0 +1,17 @@
+package com.luisbilecki.apigateway.client
+
+import com.luisbilecki.apigateway.dto.CustomerResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+
+
+
+@FeignClient(name = "customerClient", url = "\${customerClient.url}")
+interface CustomerClient {
+
+    @RequestMapping(method = [RequestMethod.GET], value = ["/customers/{cpf}"])
+    fun getCustomer(@PathVariable("cpf") cpf: String): CustomerResponse?
+}
---
+++ apigateway/src/main/kotlin/com/luisbilecki/apigateway/dto/CustomerResponse.kt
@@ -0,0 +1,3 @@
+package com.luisbilecki.apigateway.dto
+
+data class CustomerResponse(val cpf: String, val name: String)
